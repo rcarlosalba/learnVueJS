e6b0cfb04e91511f4e774426ae79574b3e9b30d2
Carlos Alba <hola@carlosalba.net>
2020-03-22 14:28:52 +0200

    Hola Vue

---
+++ code.js
@@ -0,0 +1,14 @@
+// let title = "Hola HTML"
+// const h1 = document.getElementById("h1")
+// h1.innerText = ("Hola Vue")
+
+
+new Vue({
+  el: "#app",
+
+  data(){
+    return{
+      title: "Hola de nuevo Vue!"
+    }
+  }
+})
