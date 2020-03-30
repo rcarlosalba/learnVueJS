7b3ecbf474ba9d45c4c64989a8e34d8c4c916b7a
Carlos Alba <hola@carlosalba.net>
2020-03-30 09:20:43 +0300

    Nuevos apuntes

--- code.js
+++ code.js
@@ -20,7 +20,26 @@ new Vue({
     return{
       title: "Bitcoin",
       name: "Bitcoin",
-      img: ("https://cryptologos.cc/logos/bitcoin-btc-logo.png")
+      img: ("https://cryptologos.cc/logos/bitcoin-btc-logo.png"),
+      changePercent: 10,
+      price: 8500,
+      prices: [100,200,300,400,220,541,258,230,540], 
+      titlePrice: "Precio del Bitcoin",
+      pricesWithDays:[
+        {day:"Lunes", value:200},
+        {day:"Martes", value:500},
+        {day:"Miércoles", value:800},
+        {day:"Jueves", value:8500},
+        {day:"Viernes", value:300},
+        {day:"Sábado", value:580},
+        {day:"Domingo", value:9850}
+      ], 
+      showPrices: false,
+    }
+  },
+  methods:{
+    toggleShowPrices(){
+      this.showPrices = !this.showPrices
     }
   }
 })
